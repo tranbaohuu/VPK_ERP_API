70f6e72446efe5a24eb58dc4632ee7a5c0061009
Tran Bao Huu <tranbaohuu@gmail.com>
2020-08-14 21:13:25 +0700

    Update ReceiptController.cs
    lam 2

--- VPK_ERP_API/Controllers/ReceiptController.cs
+++ VPK_ERP_API/Controllers/ReceiptController.cs
@@ -32,14 +32,35 @@ namespace VPK_ERP_API.Controllers
                 s.RowID,
                 s.Code,
                 s.Description,
-                s.CreatedDate,
-                ListOfReceipLine = s.ReceiptLines.Select(s2 => new { s2.RowID, s2.Description, s2.RowIDContract }).ToList()
+                s.CreatedDate
+                //ListOfReceipLine = s.ReceiptLines.Select(s2 => new { s2.RowID, s2.Description, s2.RowIDContract }).ToList()
 
 
             }).OrderByDescending(o => o.RowID).ToList();
 
 
 
+
+            return Ok(listOfReceiptHeaderAndLine);
+
+        }
+
+
+
+        [HttpPost]
+        //[ResponseType(typeof(Building))]
+        [Route("api/danh-sach-chi-tiet-phieu")]
+        public IHttpActionResult DanhSachChiTietPhieuTheoRowIDReceiptHeader(ReceiptHeader c)
+        {
+            var listOfReceiptHeaderAndLine = db.ReceiptLines.Where(w => w.RowID == c.RowID).ToList().Select(s => new
+            {
+                s.RowID,
+                s.Description,
+                s.Times,
+                s.TotalPrice,
+                s.CreatedDate
+
+            }).OrderByDescending(o => o.RowID).ToList();
 
             return Ok(listOfReceiptHeaderAndLine);
 
